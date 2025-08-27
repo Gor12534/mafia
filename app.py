eac04158cbbcc7d335de9868e443fa7c07af6069
Gor12534 <shop.sup000@gmail.com>
2025-08-27 19:44:59 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,4 @@
-from fastapi import FastAPI, Request
-import os
-from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes
+from bot import run_bot
 
-app = FastAPI()
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-application = ApplicationBuilder().token(BOT_TOKEN).build()
-
-@app.post("/webhook")
-async def webhook(req: Request):
-    data = await req.json()
-    update = Update.de_json(data, application.bot)
-    await application.update_queue.put(update)
-    return {"status": "ok"}
+if __name__ == "__main__":
+    run_bot()
